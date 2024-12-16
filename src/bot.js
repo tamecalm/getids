2ba1c0d8234f421a08f62487b68555b63339c8d2
John <plumecalm@gmail.com>
2024-12-16 15:04:02 +0100

    shs

--- src/bot.js
+++ src/bot.js
@@ -4,6 +4,33 @@ const routeUpdates = require('./routes');
 
 const bot = new TelegramBot(botToken, { polling: true });
 
-bot.on('message', (message) => routeUpdates(bot, message));  // Route messages
+// Listen for incoming messages and route them to the appropriate handler
+bot.on('message', (message) => routeUpdates(bot, message));
+
+// Listen for callback queries (inline button presses)
+bot.on('callback_query', (callbackQuery) => {
+    const { data, message, from } = callbackQuery;
+    const { chat, message_id } = message;
+
+    try {
+        if (data === 'get_chat_id') {
+            bot.sendMessage(chat.id, `This is the ID of the current chat: <code>${chat.id}</code>`, { parse_mode: 'HTML' });
+        } else if (data === 'delete_message') {
+            bot.deleteMessage(chat.id, message_id).catch(() => {}); // Silent fail
+            bot.sendMessage(chat.id, '✅ Message deleted! Have a nice day!');
+        } else if (data === 'get_help') {
+            bot.sendMessage(chat.id, '🔧 Use /id to get the ID of this chat (user, group, or channel).');
+        } else if (data === 'get_id') {
+            bot.sendMessage(chat.id, `The ID of this <b>${chat.type}</b> is: <code>${chat.id}</code>`, {
+                parse_mode: 'HTML'
+            });
+        }
+
+        // Acknowledge callback query to remove the loading state on the button
+        bot.answerCallbackQuery(callbackQuery.id);
+    } catch (error) {
+        console.error(error); // Log error on the server side
+    }
+});
 
 module.exports = bot;
